734cc2896d366bb25d8fe61fbd2d12abbbc8cf01
G0d1y <73200403+G0d1y@users.noreply.github.com>
2024-11-23 09:37:04 +0330

    Update itt.py

--- itt.py
+++ itt.py
@@ -1,8 +1,8 @@
 import json
 import os
 from pyrogram import Client, filters
-import pytesseract
 from PIL import Image, ImageEnhance
+import pytesseract
 import cv2
 import numpy as np
 
@@ -23,14 +23,20 @@ app = Client(
 def preprocess_image(image_path):
     image = Image.open(image_path)
     
+    
     gray_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+    
     
     _, thresholded_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
     
+    
     denoised_image = cv2.fastNlMeansDenoising(thresholded_image, None, 30, 7, 21)
     
-    processed_image = Image.fromarray(denoised_image)
-    return processed_image
+    
+    denoised_image = Image.fromarray(denoised_image)
+    denoised_image = denoised_image.resize((denoised_image.width * 2, denoised_image.height * 2), Image.ANTIALIAS)
+    
+    return denoised_image
 
 @app.on_message(filters.photo)
 async def read_text_from_image(client, message):
@@ -39,7 +45,8 @@ async def read_text_from_image(client, message):
     try:
         processed_image = preprocess_image(file_path)
         
-        extracted_text = pytesseract.image_to_string(processed_image, lang='fas')
+        custom_oem_psm_config = r'--oem 3 --psm 6'
+        extracted_text = pytesseract.image_to_string(processed_image, lang='fas', config=custom_oem_psm_config)
         
         if extracted_text.strip():
             await message.reply_text(f"Extracted Text:\n\n{extracted_text}")
@@ -54,7 +61,7 @@ async def read_text_from_image(client, message):
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
     await message.reply_text(
-        "Hello! Send an image with the to extract Persian text from it."
+        "Hello! Send an image with Persian text to extract it."
     )
 
 app.run()
